8f5d4bede42e9bbdb423175b0236de22ab16b5bd
Jays-hub <josepht.professional@gmail.com>
2024-09-13 12:08:13 -0400

    Jays update
    Google places API introduced

--- Yelp_Project/settings.py
+++ Yelp_Project/settings.py
@@ -11,6 +11,10 @@ https://docs.djangoproject.com/en/5.1/ref/settings/
 """
 
 from pathlib import Path
+import os
+
+# Google places API
+GOOGLE_PLACES_KEY = 'AIzaSyAs8a1lTOx0EEvlEy5C-CpuyrR2_Jy7Y3g'
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
